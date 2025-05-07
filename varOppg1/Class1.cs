b337af772766a02ec7914bea6e3197db216feb55
CsLy0 <marius.sundt.skullestad@gmail.com>
2025-05-07 14:36:42 +0200

    ds

--- varOppg1/Class1.cs
+++ varOppg1/Class1.cs
@@ -5,5 +5,5 @@ Console.WriteLine($"a = {inputInt}");
 var inputDecimal = 3.01;
 Console.WriteLine($"b = {inputDecimal}");
 
-var inputSum = 8.01;
+double inputSum = 8.01; //eller bruke var?
 Console.WriteLine($"sum ({inputSum}) = {inputInt} + {inputDecimal}");
