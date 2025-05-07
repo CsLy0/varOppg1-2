69d20a5f744e3b4173157c7815c4c2bd5380a476
CsLy0 <marius.sundt.skullestad@gmail.com>
2025-05-07 14:25:24 +0200

    Add project files.

---
+++ varOppg1/Class1.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Velkommen til oppg2");
+int inputInt = 5;
+Console.WriteLine($"a = {inputInt}");
+
+var inputDecimal = 3.01;
+Console.WriteLine($"b = {inputDecimal}");
+
+var inputSum = 8.01;
+Console.WriteLine($"sum ({inputSum}) = {inputInt} + {inputDecimal}");
---
+++ varOppg1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace varOppg1
+{
+    internal class Datatyper
+    {
+
+        int tall = 1;
+
+        float desimalTall = 1.01F; //F = nøyaktig
+        double desimalTall2 = 1.01;
+        decimal desimalTall3 = 1.01M; //M = veldig nøyaktig
+
+        char bokstav = 'a';
+        string tekst = "Hei";
+
+        bool santUsant = true; //eller false
+
+        int[] tallArray = { 1, 2, 3 };
+        string[] tekstArray = { "Hei", "på", "deg" };
+
+        List<int> listeMedTall = new List<int>(); //lager liste med tall
+
+        public int NumberFive()
+        {
+            return 5;
+        }
+    }
+}
+
