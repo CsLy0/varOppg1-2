dee70bc87bebccc35c64b0e740b4b2ba2ece793f
CsLy0 <marius.sundt.skullestad@gmail.com>
2025-05-08 08:19:38 +0200

    dsa

--- varOppg1/Class1.cs
+++ varOppg1/Class1.cs
@@ -2,7 +2,7 @@
 int inputInt = 5;
 Console.WriteLine($"a = {inputInt}");
 
-var inputDecimal = 3.01;
+decimal inputDecimal = 3.01M;
 Console.WriteLine($"b = {inputDecimal}");
 
 double inputSum = 8.01; //eller bruke var?
